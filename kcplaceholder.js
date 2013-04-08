b8c663cc582150fd797d61f56f8cc91d0097c808
Kevin Crider <kevinacrider@gmail.com>
2013-04-08 17:30:46 -0400

    Initial Commit

---
+++ kcplaceholder.js
@@ -0,0 +1,21 @@
+$(function(){
+
+	// Remove placeholder when the element is brought into focus
+  $('[placeholder]').focus(function() {
+    var element = $(this), value = element.val(), placeholder = element.attr('placeholder');
+    if (value == placeholder) {element.val('');}
+
+  // Put the placeholder text back into the element if it's empty
+	}).blur(function() {
+    var element = $(this), value = element.val(), placeholder = element.attr('placeholder');
+    if (value == '' || value == placeholder) {element.val(placeholder);}
+
+  // Before the form is submitted, we need to remove all our placeholder text so it does not get submitted with the form
+	}).blur().parents('form').submit(function() {
+    var element = $(this), value = element.val(), placeholder = element.attr('placeholder');
+    element.find('[placeholder]').each(function() {
+			if (value == placeholder) {element.val('');}
+		})
+	});
+
+});
